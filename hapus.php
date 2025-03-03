790b99cb1fdb8c5cb8476b0c74b99d295fc88dd8
Aisyah Thalita Shaqirah <165042425+skiiraw@users.noreply.github.com>
2025-03-03 10:16:04 +0700

    Update hapus.php

--- hapus.php
+++ hapus.php
@@ -1,22 +1,26 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+// Connect to database
+$servername = "database-1.csqsi5wnwzc1.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "admin123";
 $dbname = "rds";
-
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
 
-// sql to delete a record
-$sql = "DELETE FROM students where nomor=1";
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get ID from URL
+$id = $_GET['id'];
+
+// Delete query
+$sql = "DELETE FROM students WHERE id=$id";
+
 if ($conn->query($sql) === TRUE) {
-  //echo "Record deleted successfully";
-  echo "<script>alert('Record deleted successfully');location.href='index.php'</script>";
+    echo "<script>alert('Data deleted successfully');location.href='index.php'</script>";
 } else {
-  echo "Error deleting record: " . $conn->error;
+    echo "Error deleting record: " . $conn->error;
 }
 
 $conn->close();
